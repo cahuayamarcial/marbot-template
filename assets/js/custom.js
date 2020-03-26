0c7cab40388f3ac2b3fbadd54071d97895f90a88
cahuayamarcial <cahuaya.marcial@gmail.com>
2020-03-26 12:40:25 -0400

    Image App + Icons 🍔

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,9 +58,9 @@ jQuery(function($) {
 
 
     // Helper function for add element box list in WOW
-    WOW.prototype.addBox = function(element) {
-        this.boxes.push(element);
-    };
+    // WOW.prototype.addBox = function(element) {
+    //     this.boxes.push(element);
+    // };
 
     // Init WOW.js and get instance
     var wow = new WOW();
@@ -68,13 +68,13 @@ jQuery(function($) {
 
     // Attach scrollSpy to .wow elements for detect view exit events,
     // then reset elements and add again for animation
-    $('.wow').on('scrollSpy:exit', function() {
-        $(this).css({
-            'visibility': 'hidden',
-            'animation-name': 'none'
-        }).removeClass('animated');
-        wow.addBox(this);
-    }).scrollSpy();
+    // $('.wow').on('scrollSpy:exit', function() {
+    //     $(this).css({
+    //         'visibility': 'hidden',
+    //         'animation-name': 'none'
+    //     }).removeClass('animated');
+    //     wow.addBox(this);
+    // }).scrollSpy();
 
     // Initial Page
     $(document).ready(function() {
