6eafd76bc5718231b16c9de837c57d89b0029175
cahuayamarcial <cahuaya.marcial@gmail.com>
2020-04-29 15:24:41 -0400

    contact 🐱

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,55 +29,109 @@ jQuery(function($) {
     });
 
 
-    // jQuery for Easing min
-    (function($) {
-        "use strict";
-        // Smooth scrolling using jQuery easing
-        $('a.marbot-scroll-trigger[href*="#"]:not([href="#"])').on('click', function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: (target.offset().top - 54)
-                    }, 1000, "easeInOutExpo");
-                    return false;
-                }
+    $('a.marbot-scroll-trigger[href*="#"]:not([href="#"])').on('click', function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
             }
-        });
-        // Closes responsive menu when a scroll trigger link is clicked
-        $('.marbot-scroll-trigger').on('click', function() {
-            $('.navbar-collapse').collapse('hide');
-            
-        });
-        // Activate scrollspy to add active class to navbar items on scroll
-        $('body').scrollspy({
-            target: '#mainNav',
-            offset: 54
-        });
+        }
+    });
 
-    })(jQuery); // End of use strict
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.marbot-scroll-trigger').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+        
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
 
     // Init WOW.js and get instance
     var wow = new WOW();
     wow.init();
     
-    AOS.init()
-    
+    AOS.init();
 
-});
+    $(window).on ('load', function (){
+        // Preloader
+        $('#ctn-preloader').fadeOut(); 
+        $('#preloader').delay(350).fadeOut('slow'); 
+        $('body').delay(350).css({'overflow':'visible'});
+        // Fancybox
+        var fancy = $ (".fancybox");
+        if(fancy.length) {
+            fancy.fancybox({
+                arrows: true,
+                buttons: [
+                    "zoom",
+                    //"share",
+                    "slideShow",
+                    //"fullScreen",
+                    //"download",
+                    "thumbs",
+                    "close"
+                ],
+                animationEffect: "zoom-in-out",
+                transitionEffect: "zoom-in-out",
+            });
+        }
+    });
 
 
-// Initial Page
-$(document).ready(function() {
-    // Menu
-    $('#nav-marbot-hamburger').click(function() {
-        $(this).toggleClass('open').siblings().removeClass('open');
-        if ($('#btnMenu').attr('aria-expanded') == "true") {
-            $('.navbar').removeClass('border-navbar')
-        } else {
-            $('.navbar').addClass('border-navbar')
+
+    // Works
+
+    $(document).on ('ready', function (){
+
+        // Menu
+        $('#nav-marbot-hamburger').click(function() {
+            $(this).toggleClass('open').siblings().removeClass('open');
+            if ($('#btnMenu').attr('aria-expanded') == "true") {
+                $('.navbar').removeClass('border-navbar')
+            } else {
+                $('.navbar').addClass('border-navbar')
+            }
+        });
+
+        // Gallery Slider
+        var tSlider = $ (".gallery-slider");
+        if(tSlider.length) {
+            tSlider.owlCarousel({
+              loop:true,
+              nav:true,
+              navText: ["<i class='flaticon-back'></i>" , "<i class='flaticon-next'></i>"],
+            // navText: ["<i class='flaticon-back'><-</i>" , "<i class='flaticon-next'>-></i>"],
+              dots:false,
+              autoplay:true,
+              autoplayTimeout:4000,
+              smartSpeed:1200,
+              autoplayHoverPause:true,
+              lazyLoad:true,
+              responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:2
+                    },
+                    1100:{
+                        items:3
+                    },
+                    1550:{
+                        items:4,
+                    }
+                },
+          });
         }
     });
 
 });
+
